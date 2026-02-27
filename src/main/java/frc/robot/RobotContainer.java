e8dfa3ec1bc2caef7ec51ea2ff3d312b0772326a
Rushali B <26rbetala@team102.org>
2026-02-27 18:15:09 -0500

    changed button bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -221,7 +221,6 @@ public class RobotContainer {
     //running motors
     operatorXbox.leftTrigger().whileTrue(new RunIndexer(indexer));
     operatorXbox.leftBumper().whileTrue(new RunFeeder(indexer));
-    operatorXbox.povLeft().whileTrue(new RunShooter(shooter));
     //operatorXbox.rightBumper().whileTrue(new BasicShooter(shooter,ShooterConstants.FRONT_TRENCH));
     //operatorXbox.povRight().whileTrue(new BasicShooter(shooter,ShooterConstants.FRONT_TOWER));
     operatorXbox.y().whileTrue(new BasicShooter(shooter,() -> Robot.ShooterSpeed.getDouble(-4000)));
@@ -236,8 +235,15 @@ public class RobotContainer {
     //operatorXbox.y().whileTrue(new FullFuelCycle(shooter, indexer, intake));
     operatorXbox.rightTrigger().whileTrue(new IntakeFuel(intake, () -> Robot.IntakeSpeed.getDouble(Constants.IntakeConstants.INTAKE_DEFAULT_SPEED)));
     operatorXbox.rightTrigger().whileFalse(new IdleIntake(intake));
-    operatorXbox.povDown().onTrue(new RunClimber(climber));
     operatorXbox.povUp().whileTrue(new AllianceCheck(shooter, drivebase, indexer));
+
+
+
+
+
+
+
+
 
     testerXbox.a().whileTrue(shooter.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
     testerXbox.b().whileTrue(shooter.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
