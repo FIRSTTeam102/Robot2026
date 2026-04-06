6b0cea62ea9583fca681ec9100b9e5234b608a08
Joe M <26jmurdock@team102.org>
2026-04-06 18:25:03 -0400

    logged other actuator

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -142,6 +142,11 @@ public class Shooter extends SubsystemBase {
    }
 
    @AutoLogOutput
+   public double getFollowerPosition() {
+    return ((followerActuator.getPosition()*1.5625)-0.296875);
+   }
+
+   @AutoLogOutput
    public double shooterCurrent() {
     return shooterMotor.getOutputCurrent();
    }
