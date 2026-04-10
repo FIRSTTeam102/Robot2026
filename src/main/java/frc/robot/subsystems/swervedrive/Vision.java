4b844b7f711d61e7812ba41dbd04423289a6b289
Joe M <26jmurdock@team102.org>
2026-04-09 20:35:20 -0400

    Camera Positions

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -350,10 +350,10 @@ public class Vision
               VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
 
     BACKLEFT_CAM("BL",
-             new Rotation3d(0, Math.toRadians(0), Math.toRadians(135)),
-             new Translation3d(Units.inchesToMeters(-12.411),
-                               Units.inchesToMeters(8.324),
-                               Units.inchesToMeters(11.795)),
+             new Rotation3d(0, Math.toRadians(0), Math.toRadians(180)),
+             new Translation3d(Units.inchesToMeters(-11.822),
+                               Units.inchesToMeters(9.493),
+                               Units.inchesToMeters(18.685)),
              VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
   
     BACKRIGHT_CAM("BR",
